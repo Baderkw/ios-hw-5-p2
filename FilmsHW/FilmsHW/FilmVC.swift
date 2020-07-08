b034fb7d049b92878edea3a82e19ab31ad51d2fa
Baderkw <66323674+Baderkw@users.noreply.github.com>
2020-07-09 00:19:26 +0300

    finshed

--- FilmsHW/FilmsHW/FilmVC.swift
+++ FilmsHW/FilmsHW/FilmVC.swift
@@ -7,15 +7,16 @@
 //
 
 import UIKit
+import AVFoundation
 
 class FilmVC: UIViewController {
 
+    @IBOutlet weak var categoryLabel: UILabel!
     
     @IBOutlet weak var filmImage: UIImageView!
-    
     @IBOutlet weak var castLabel: UILabel!
-    
     @IBOutlet weak var ratingLabel: UILabel!
+    var themeSong: AVAudioPlayer?
     
     var film = Film(name: "", rating: "", pgRating: "", actors: "", categories: "")
     
@@ -24,6 +25,23 @@ class FilmVC: UIViewController {
         filmImage.image = UIImage(named: film.name)
         castLabel.text = film.actors
         ratingLabel.text = film.rating
+        categoryLabel.text = film.categories
+        playSound()
+    }
+
+    func playSound() {
+        let url = Bundle.main.url(forResource: film.name, withExtension: "mp3")!
+
+        do {
+            themeSong = try AVAudioPlayer(contentsOf: url)
+            guard let player = themeSong else { return }
+
+            player.prepareToPlay()
+            player.play()
+
+        } catch let error as NSError {
+            print(error.description)
+        }
     }
     
 }
